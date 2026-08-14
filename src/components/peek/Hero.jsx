bd1d3179b9506319086cf578de8b17cca46cadd2
osevastyanovadesign-ctrl <o.sevastyanova.design@gmail.com>
2026-08-14 11:20:07 +0300

    Update Hero.jsx

--- src/components/peek/Hero.jsx
+++ src/components/peek/Hero.jsx
@@ -21,12 +21,12 @@ const HERO_GALLERY = [
 ];
 
 const POLAROID_POSES = [
-  { rotate: 5, x: 0, y: -4, scale: 1 },
-  { rotate: -7, x: -10, y: 8, scale: 0.99 },
-  { rotate: 4, x: 8, y: -2, scale: 1.01 },
-  { rotate: -5, x: -5, y: 10, scale: 0.99 },
-  { rotate: 7, x: 5, y: -6, scale: 1 },
-  { rotate: -4, x: -8, y: 4, scale: 1.01 },
+  { rotate: 5,  x: 0,   y: -18, scale: 1 },
+  { rotate: -7, x: -12, y: 14,  scale: 0.99 },
+  { rotate: 4,  x: 10,  y: -8,  scale: 1.01 },
+  { rotate: -5, x: -8,  y: 20,  scale: 0.99 },
+  { rotate: 7,  x: 6,   y: -14, scale: 1 },
+  { rotate: -4, x: -10, y: 10,  scale: 1.01 },
 ];
 
 export default function Hero() {

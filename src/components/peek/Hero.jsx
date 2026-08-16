afa83a7ba6c07db7f9090acdeb6da3bb34c62021
osevastyanovadesign-ctrl <o.sevastyanova.design@gmail.com>
2026-08-16 14:50:34 +0300

    Update Hero.jsx

--- src/components/peek/Hero.jsx
+++ src/components/peek/Hero.jsx
@@ -491,7 +491,7 @@ onDragEnd={(event, info) => {
         : isLifting
         ? {
             opacity: 1,
-            x: "-10vw",
+            x: "-15vw",
             y: 0,
             rotate: 0,
             scale: 1.28,
